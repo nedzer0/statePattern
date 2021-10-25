ffa49ef01f18c1b221fd76ec1cb6a0ec81b3b535
João <jp.felix@campus.fct.unl.pt>
2021-10-25 23:13:12 +0100

    método doAction() feito.

--- src/TurnOffState.java
+++ src/TurnOffState.java
@@ -1,11 +1,11 @@
 public class TurnOffState implements  StateInterface {
 
     public TurnOffState() {
-
     }
 
     @Override
     public void doAction(VendingMachine vm) {
-
+        vm.setState(this);
+        System.out.println("Vending machine turned OFF!");
     }
 }
--- src/TurnOnState.java
+++ src/TurnOnState.java
@@ -1,12 +1,12 @@
 public class TurnOnState implements  StateInterface {
 
     public TurnOnState() {
-
     }
 
 
     @Override
     public void doAction(VendingMachine vm) {
-
+        vm.setState(this);
+        System.out.println("Vending machine turned ON!");
     }
 }
