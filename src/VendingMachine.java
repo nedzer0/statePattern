fdbc203d056a1c61331ee3829ac39bc574dc0529
André Cordeiro <am.cordeiro@campus.fct.unl.pt>
2021-10-25 15:30:42 +0100

    Updated VendingMachine Class

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,10 +1,14 @@
 public class VendingMachine {
-
+    private StateInterface state;
     public VendingMachine() {
-
+        this.state = null;
     }
 
     public void setState(StateInterface s) {
+        this.state = s;
+    }
 
+    public StateInterface getState(){
+        return state;
     }
 }
